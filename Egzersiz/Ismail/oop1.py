53e5c5ab3ee4d147bfa5136ffaf69875e54db6f4
İbrahim EDİZ <edizdesign@gmail.com>
2022-03-18 17:12:30 +0000

    18032022

---
+++ Egzersiz/Ismail/oop1.py
@@ -0,0 +1,2 @@
+merhaba
+print("merhaba")
--- folder.py
+++ folder.py
@@ -1,6 +1,11 @@
 liste = ["Ahmet","Dilber","Erdal","Fatma","Filiz","Yahya","Huseyin","Ismail","Nida","Orhan","Rabia","Sevgi","Yakup"]
 
+folder="Notlar"
+filename = "oop1"
 import os 
 for item in liste:
-    os.mkdir(f"Egzersiz/{item}")
-    open(f"Egzersiz/{item}/oop1.py","w+")
+    if not os.path.exists(f"{folder}"):
+        os.mkdir(folder)
+    if not os.path.exists(f"{folder}/{item}"):
+        os.mkdir(f"{folder}/{item}")
+    open(f"{folder}/{item}/oop1.py","a+")
